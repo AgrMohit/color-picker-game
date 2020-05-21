bff9af7330e93d57d12c04705bde3bd18420be3e
Mohit Raj <agrmohit@outlook.com>
2020-05-21 23:34:32 +0530

    Test space requirement with most UI elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React from 'react';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <p>Hello</p>
+    <div className='main-box'>
+      <div className='message-box default-choice'>hsl(120, 50%, 50%)</div>
+      <div className='message-box correct-choice'>Correct Answer</div>
+      <div className='message-box wrong-choice'>Wrong Answer</div>
+      <div className='color-box slateblue'></div>
+      <div className='color-box aquamarine'></div>
+      <div className='color-box salmon'></div>
+      <div className='color-box cornflowerblue'></div>
+      <div className='color-box palegreen'></div>
     </div>
   );
 };
